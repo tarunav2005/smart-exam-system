2ca94aff0a9362196e5e86132f642b3b7f30a008
Tarun Kumar <kumartarun7145@gmail.com>
2026-07-18 22:03:20 +0530

    Fix User model import case

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Checks if a valid JWT was sent, attaches the user to req
 const protect = async (req, res, next) => {
